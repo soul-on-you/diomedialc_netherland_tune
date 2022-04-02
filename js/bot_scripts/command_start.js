421cf2799749f263165f947d838d2804cb0524a6
brendan <superden081@gmail.com>
2022-04-03 00:07:20 +0300

    new bot scripts start and later info

---
+++ js/bot_scripts/command_start.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default async function (chatID, UsersModel, bot, msg) {
+  try {
+    if (chatID == process.env.ADMIN_ID) {
+      await UsersModel.create({ chatID: chatID, rules: "ADMIN" });
+    } else {
+      await UsersModel.create({ chatID: chatID });
+    }
+  } catch (e) {
+    if (e.name != "SequelizeUniqueConstraintError") {
+      const eMsg =
+        await "Неудалось создать запись нового пользователя в БД! попробуйте позже!\n";
+      await console.error(`${eMsg}\n${chatID}\n${e}\n`);
+      console.log(e.name);
+      return bot.sendMessage(chatID, eMsg);
+    }
+  }
+  await bot.sendSticker(
+    chatID,
+    `CAACAgIAAxkBAAMnYiouYynylMK9Uv6l5pi5p6VJU8MAAqkAA0aSAS9evFTSF6HhoiME`
+  );
+  return bot.sendMessage(
+    chatID,
+    `Привет! ${msg.from.first_name} ${msg.from.last_name}`
+  );
+}
---
+++ js/bot_scripts/index.js
@@ -0,0 +1,5 @@
+// export { default as start } from "./command_start";
+// export { default as info } from "./command_info";
+import start from "./command_start";
+import info from "./command_info";
+export default { start, info };
