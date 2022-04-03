e10901c868c6f7c40aad549f601f4baad5ee135c
brendan <superden081@gmail.com>
2022-04-03 17:04:05 +0300

    add info command, later need add error logger

--- js/bot_scripts/command_info.js
+++ js/bot_scripts/command_info.js
@@ -1,3 +1,35 @@
-export default function () {
-  return console.log("info called");
+import BotOptions from "../bot_options";
+// BotOprions.adminActions
+// {
+//   adminActions,
+//   moderatorActions,
+//   userActions,
+// }
+
+export default async function (chatID, UsersModel, bot, msg) {
+  try {
+    const user = await UsersModel.findOne({ where: { chatID: chatID } });
+    let options;
+    switch (user.rules) {
+      case "ADMIN":
+        options = BotOptions.adminActions;
+        break;
+      case "MODERATOR":
+        options = BotOptions.moderatorActions;
+        break;
+      case "USER":
+        options = BotOptions.userActions;
+        break;
+    }
+    return bot.sendMessage(
+      chatID,
+      `Приветствую, ${msg.from.first_name}!\nВыберите действие:`,
+      options
+    );
+  } catch (e) {
+    const eMsg =
+      "Системная ошибка. Подключение к БД не выполнилось!\nПовторите свои действия позже\n";
+    console.error(`${eMsg}\n${e}\n`);
+    return bot.sendMessage(chatID, eMsg);
+  }
 }
