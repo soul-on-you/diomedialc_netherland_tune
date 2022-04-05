7cef3603b50a14caef28365cbb8f7b9254c1e8f9
brendan <superden081@gmail.com>
2022-04-05 20:08:50 +0300

    added options to back to choose edit abilities with servers

--- js/bot_scripts/callbacks/editServerInfo/callback_backToAdminEditServer.js
+++ js/bot_scripts/callbacks/editServerInfo/callback_backToAdminEditServer.js
@@ -1,3 +1,11 @@
-export default async function () {
-  console.log("backToAdminEditServer");
+import BotOptions from "../../../bot_options";
+
+export default function (bot, chatID, messageID, states) {
+  states[chatID] = {};
+
+  return bot.editMessageText("Что вы хотите редактировать", {
+    chat_id: chatID,
+    message_id: messageID,
+    ...BotOptions.adminActionEditServer,
+  });
 }
