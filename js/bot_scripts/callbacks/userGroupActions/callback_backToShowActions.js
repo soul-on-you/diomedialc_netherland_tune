c3e0e87e468cc28816bac92edd057a44d5169194
brendan <superden081@gmail.com>
2022-04-05 20:09:42 +0300

    added options all users to back in main bot message

--- js/bot_scripts/callbacks/userGroupActions/callback_backToShowActions.js
+++ js/bot_scripts/callbacks/userGroupActions/callback_backToShowActions.js
@@ -1,3 +1,34 @@
-export default async function () {
-  console.log("backToShowActions");
+import BotOptions from "../../../bot_options";
+import { UserRulesResolver } from "../../../services";
+
+export default async function (bot, chatID, messageID, first_name, UsersModel) {
+  try {
+    const options = await UserRulesResolver(
+      UsersModel,
+      chatID,
+      BotOptions.adminActions,
+      BotOptions.moderatorActions,
+      BotOptions.userActions
+    );
+
+    return bot.editMessageText(
+      `Приветствую, ${first_name}!\nВыберите действие:`,
+      {
+        chat_id: chatID,
+        message_id: messageID,
+        ...options,
+      }
+    );
+  } catch (e) {
+    const eMsg =
+      "Системная ошибка. Подключение к БД не выполнилось!\nПовторите свои действия позже\n";
+    console.error(`${eMsg}\n${e}\n`);
+    return bot.sendMessage(chatID, eMsg);
+  }
 }
+
+// return bot.sendMessage(
+//   chatID,
+//   `Приветствую, ${first_name}!\nВыберите действие:`,
+//   options
+// );
