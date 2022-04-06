61345372db3d921750e2c009f9867b5c53087070
brendan <superden081@gmail.com>
2022-04-06 23:15:24 +0300

    added bot qiwi merchant order open

--- js/bot_scripts/callbacks/orderPaymentMethod/callback_orderQiwiPay.js
+++ js/bot_scripts/callbacks/orderPaymentMethod/callback_orderQiwiPay.js
@@ -1,3 +1,37 @@
-export default async function () {
-  console.log("orderQiwiPay");
+import BotOptions from "../../../bot_options";
+import { CalculateEndOfLeaseDate } from "../../../services";
+import { QiwiPayMerchant } from "../../../payment";
+
+export default async function (
+  bot,
+  chatID,
+  messageID,
+  states,
+  billIDs,
+  localTunnels
+) {
+  const tunnel = localTunnels.find(
+    (tunnel) => tunnel.serverID == states[chatID].serverID
+  );
+
+  const orderPrice = tunnel.price * states[chatID].orderCount;
+
+  const [billID, payUrl] = await QiwiPayMerchant(orderPrice);
+
+  billIDs.push(billID);
+
+  states[chatID] = { ...states[chatID], billID: billID, payUrl: payUrl };
+
+  bot.editMessageText(
+    `\u{1F4B3} Оплата товара\n\n\u{1F4E1} Товар: ${
+      tunnel.serverName
+    }\n\n\u{1F4B5}Сумма к оплате: ${orderPrice}₽\n\n\u{1F5D3} Подписка до ${CalculateEndOfLeaseDate(
+      states[chatID].orderCount
+    )} `,
+    {
+      chat_id: chatID,
+      message_id: messageID,
+      ...BotOptions.allActionPayOrder(payUrl, orderPrice),
+    }
+  );
 }
