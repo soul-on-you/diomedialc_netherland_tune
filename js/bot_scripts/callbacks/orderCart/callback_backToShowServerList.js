404c82de982e3e7860922f6b7c0e4e35d0c299c4
brendan <superden081@gmail.com>
2022-04-06 21:27:06 +0300

    added abilities to continue pay and get paymethod AND back to browse server list

--- js/bot_scripts/callbacks/orderCart/callback_backToShowServerList.js
+++ js/bot_scripts/callbacks/orderCart/callback_backToShowServerList.js
@@ -1,3 +1,38 @@
-export default async function () {
-  console.log("backToShowServerList");
+import BotOptions from "../../../bot_options";
+import { InternetSpeed, EmojiFlag } from "../../../services";
+
+export default function (bot, chatID, messageID, states, localTunnels) {
+  states[chatID] = {};
+
+  const message_buttons = localTunnels.map((tunnel) => {
+    return [
+      {
+        text: `${EmojiFlag(tunnel.emojiCountryCode)}${
+          tunnel.serverName
+        } ${String.fromCharCode(55356)}${String.fromCharCode(
+          57104
+        )} ${InternetSpeed(tunnel.maxSpeed)} ${tunnel.price}₽`,
+        callback_data: tunnel.serverID,
+      },
+    ];
+  });
+
+  const message_options_reply_markup = JSON.stringify({
+    inline_keyboard: [
+      ...message_buttons,
+      ...JSON.parse(BotOptions.allBackToMain.reply_markup).inline_keyboard,
+    ],
+  });
+
+  states[chatID] = {
+    runtime: "callback",
+    command: "orderCart",
+    messageID: messageID,
+  };
+
+  bot.editMessageText("\u{1F4E1} Выберите сервер, который хотите арендовать:", {
+    chat_id: chatID,
+    message_id: messageID,
+    reply_markup: message_options_reply_markup,
+  });
 }
--- js/bot_scripts/callbacks/orderCart/callback_orderShowPayMethods.js
+++ js/bot_scripts/callbacks/orderCart/callback_orderShowPayMethods.js
@@ -1,3 +1,29 @@
-export default async function () {
-  console.log("orderShowPayMethods");
+import BotOptions from "../../../bot_options";
+import { CalculateEndOfLeaseDate } from "../../../services";
+
+export default function (bot, chatID, messageID, states, localTunnels) {
+  const tunnel = localTunnels.find(
+    (tunnel) => tunnel.serverID == states[chatID].serverID
+  );
+  console.log(states);
+
+  bot.editMessageText(
+    `\u{1F4B3} Способ оплаты\n\n\u{1F4E1} Товар: ${
+      tunnel.serverName
+    }\n\n\u{1F4B5}Сумма к оплате: ${
+      tunnel.price * states[chatID].orderCount
+    }₽\n\n\u{1F5D3} Подписка до ${CalculateEndOfLeaseDate(
+      states[chatID].orderCount
+    )} `,
+    {
+      chat_id: chatID,
+      message_id: messageID,
+      ...BotOptions.allActionChoosePayMethod,
+    }
+  );
 }
+
+//1F4B3 credit card
+//1F4E1 antena
+//1F4B5 dollar banknote
+//1F5D3 spiral calendar
