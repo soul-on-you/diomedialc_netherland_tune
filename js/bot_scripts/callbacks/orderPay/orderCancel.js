15e796ddc09638a7279f8729bf16f699686fffa4
brendan <superden081@gmail.com>
2022-04-06 23:15:53 +0300

    added qiwi merchant order close

--- js/bot_scripts/callbacks/orderPay/orderCancel.js
+++ js/bot_scripts/callbacks/orderPay/orderCancel.js
@@ -1,4 +1,22 @@
-export default async function () {
-  console.log("orderCancel");
+import sendInfoMessage from "../../commands/base/command_info"
+import { QiwiCloseOrder } from "../../../payment";
+
+export default async function (bot, chatID, messageID, states, billIDs, first_name, UsersModel) {
+  const billID = states[chatID].billID;
+
+  QiwiCloseOrder(billID);
+
+  billIDs.splice(billIDs.indexOf(billID), 1);
+
+  states[chatID] = {};
+
+  bot.editMessageText("\u267B Заказ удален", {
+    chat_id: chatID,
+    message_id: messageID,
+  });
+
+  await sendInfoMessage(bot, chatID, first_name, UsersModel);
+  
 }
-//тут можно вызвать отмену и потом backToShowActions.js
+
+//267B - recycling
