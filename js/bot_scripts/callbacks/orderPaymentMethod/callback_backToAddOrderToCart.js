89c1856adf13bafd955f5c4b3a085f28bf0511ce
brendan <superden081@gmail.com>
2022-04-17 23:19:28 +0300

    added options to back in order cart adnd edit order

--- js/bot_scripts/callbacks/orderPaymentMethod/callback_backToAddOrderToCart.js
+++ js/bot_scripts/callbacks/orderPaymentMethod/callback_backToAddOrderToCart.js
@@ -1,3 +1,34 @@
-export default async function () {
-  console.log("backToAddOrderToCart");
+import BotOptions from "../../../bot_options";
+import { InternetSpeed } from "../../../services";
+import { countryCodeEmoji } from "country-code-emoji";
+
+export default function (bot, chatID, messageID, states, localTunnels) {
+  const tunnel = localTunnels.find(
+    (tunnel) => tunnel.serverID == states[chatID].serverID
+  );
+
+  // states[chatID] = {
+  //   serverID: tunnel.serverID,
+  //   orderCount: 1,
+  // };
+
+  bot.editMessageText(
+    `\u{1F4E1} Сервер: ${countryCodeEmoji(tunnel.emojiCountry)} ${
+      tunnel.serverName
+    }\n\n\u{1F4B5} Цена: ${
+      tunnel.price
+    }₽/месяц\n\n\u23F1 Скорость: ${InternetSpeed(tunnel.maxSpeed)}`,
+    {
+      chat_id: chatID,
+      message_id: messageID,
+      ...BotOptions.allActionAddOrderToCart(
+        states[chatID].orderCount,
+        states[chatID].orderCount * tunnel.price
+      ),
+    }
+  );
 }
+
+//1F4E1 antena
+//1F4B5 dollar banknote
+//23F1 - stopwatch
